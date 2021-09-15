bafb5a4085e09a1917cd1eae062771e53fc8b458
AnaComBia <anabeatrizetorres@gmail.com>
2021-09-15 11:45:49 -0300

    finaliza programa Area Do Circulo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n---Irei calcular a area de uma circuferência---\n");
+
+            Console.Write("Infrome o valor do raio: ");
+            double Raio = Convert.ToDouble(Console.ReadLine());
+
+            double Area = (Math.PI) * (Math.Pow(Raio, 2));
+
+            Console.WriteLine($"Area da Circuferência = {Area}");
+
+        }
+    }
+}
